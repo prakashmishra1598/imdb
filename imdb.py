a2d8403b987008ab839061600c81c4fdf09c2d0f
Prakash Mishra <prakashmishra1598@gmail.com>
2017-05-06 06:50:43 +0000

    Initial commit

---
+++ imdb.py
@@ -0,0 +1,15 @@
+import urllib
+from bs4 import BeautifulSoup
+
+print ("Collecting data from IMDb charts....\n\n\n")
+print ("The current top 10 IMDb movies are: \n\n")
+response = urllib.request.urlopen("http://www.imdb.com/chart/top")
+html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
+mytd = soup.findAll("td", {"class":"titleColumn"})
+for titles in mytd[:10]:
+    print (titles.find('a').text)
+    
+print ("\n\nThank you for using IMDB script:) .....\n\n\n")
+    
+
