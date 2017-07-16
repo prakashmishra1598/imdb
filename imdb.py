4424c4c58282c2b767785e93ed06c8094da91d4f
Prakash Mishra <prakashmishra1598@gmail.com>
2017-07-16 05:13:47 +0000

    Updated the code
    Shows results for top 15 IMDB movies

--- imdb.py
+++ imdb.py
@@ -2,14 +2,14 @@ import urllib
 from bs4 import BeautifulSoup
 
 print ("Collecting data from IMDb charts....\n\n\n")
-print ("The current top 10 IMDB movies are: \n\n")
+print ("The current top 15 IMDB movies are the following: \n\n")
 response = urllib.request.urlopen("http://www.imdb.com/chart/top")
 html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
 mytd = soup.findAll("td", {"class":"titleColumn"})
-for titles in mytd[:10]:
+for titles in mytd[:15]:
     print (titles.find('a').text)
 #hello
-print ("\n\nThank you for using IMDB script:) .....\n\n\n")
+print ("\n\nThank you for using IMDB script :P .....\n\n\n")
     
 
